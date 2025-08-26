fa6ac5a1fad9a68873239826aa80961093d4c9d9
Yashvesh20 <yashvesh20@gmail.com>
2025-08-27 01:04:42 +0530

    Create Area_of_Rectangle.cpp

---
+++ Area_of_Rectangle.cpp
@@ -0,0 +1,27 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+#include<string>
+using namespace std;
+class rec_area{
+    public:
+    float length,breadth,area;
+    void area1();
+};
+void rec_area::area1(){
+        cout<<"Enter the length of the rectangle: "<<endl;
+        cin>>length;
+        cout<<"Enter the breadth: "<<endl;
+        cin>>breadth;
+        area=length*breadth;
+    }
+int main(){
+rec_area r1;
+r1.area1();
+cout<<"\n Area of the rectangle is : "<<r1.area<<endl;
+}
+Output:
+Enter the length of the rectangle: 
+22
+Enter the breadth: 
+20
+Area of the rectangle is : 440
